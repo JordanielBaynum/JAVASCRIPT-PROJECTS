4ff6c31b05dca58f9b836fa0ed867c46a6e73d84
JStrides77 <jordaniel.baynum@yahoo.com>
2025-05-25 13:24:33 -0700

    Adding new folders

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Hello, Quahog!"); //!This is my window alert method.//
+
+//!This is my two variables submission.//
+let Sent1 = "This is the beginning of the string";
+let Sent2 = "and this is the end of the string.";
+
+let fullsentence = Sent1 + Sent2; //!The following concatenates both strings expression.//
+
+document.write(fullsentence);
+
+
+var Family = "The Griffins", Dad = "Peter", Mom = "Lois", Daughter = "Meg", Son = "Chris"; //!Assigned var family string//
+
+document.write(Mom); //!The following prints Lois.//
+
